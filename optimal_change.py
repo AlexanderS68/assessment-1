7ddf48d15357fb5b9b9f1e9ece18326832d60185
AlexanderS68 <gruntmedic68@gmail.com>
2021-06-04 15:44:09 -0400

    revision

--- optimal_change.py
+++ optimal_change.py
@@ -1,22 +1,17 @@
+import math
 def optimal_change(item_cost, amount_paid):
-  
-#Create dictionary of currency:denomination
-#variable called amount due that holds value of amount paid - item cost
 
   currency = [100,50,20,10,5,1]
   denomination = ['Hundred', 'Fifty', 'Twenty', 'Ten', 'Five,', 'One']
 
   amount_due = amount_paid - item_cost
+  holding_ground = []
 
-  while amount_due > 0:
+  if amount_due > 0:
     for i in range(len(currency)):
-      if (amount_due >= currency[i]):
-        print(currency[i], amount_due)
-        number_of_times = amount_due // currency[i]
-        denominationhowmanytimes = denomination[i]
-        currencytimes = number_of_times * denominationhowmanytimes
-        amount_due -= currency[i]
-        print(currency[i], amount_due, number_of_times, denominationhowmanytimes, currencytimes)
+      times_how_many = math.floor(amount_due // currency[i])
+      if times_how_many > 0:
+        print(times_how_many)
+        amount_due -= times_how_many * currency[i]
 
-
-print(optimal_change(10, 30))
+print(optimal_change(10, 20))
