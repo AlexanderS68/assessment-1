8d58ea5d7d7bb9a22a210db9aee6f3d515ae74da
AlexanderS68 <gruntmedic68@gmail.com>
2021-06-04 16:24:02 -0400

    i

--- optimal_change.py
+++ optimal_change.py
@@ -3,16 +3,20 @@ def optimal_change(item_cost, amount_paid):
 
   currency = [100,50,20,10,5,1,.25,.10,.05,.01]
 
-  amount_due = amount_paid - item_cost
-  holding_ground = f'The optimal change for an item that costs ${item_cost} with an amount paid of {amount_paid} is '
+  amount_due = float(amount_paid - item_cost)
+  holding_ground = f'The optimal change for an item that costs ${item_cost} with an amount paid of ${amount_paid} is '
 
   if amount_due > 0:
     for i in range(len(currency)):
       times_how_many = math.floor(amount_due // currency[i])
       #where you account for single/plural
+      if i >= 6 and times_how_many > 0:
+        holding_ground += f'{times_how_many} ${currency[i]} cent '
+        amount_due -= (times_how_many * currency[i])
+        break
       if times_how_many > 0:
         holding_ground += f'{times_how_many} ${currency[i]} bill '
-        amount_due -= times_how_many * currency[i]
+        amount_due -= (times_how_many * currency[i])
        
   return holding_ground
-print(optimal_change(10, 20.75))
+print(optimal_change(1, 7.85))
