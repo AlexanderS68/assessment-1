5341dc839120e4bc377174da2ce7c99ede59b8b4
AlexanderS68 <gruntmedic68@gmail.com>
2021-06-04 16:04:45 -0400

    need to account for cents

--- optimal_change.py
+++ optimal_change.py
@@ -1,17 +1,18 @@
 import math
 def optimal_change(item_cost, amount_paid):
 
-  currency = [100,50,20,10,5,1]
-  denomination = ['Hundred', 'Fifty', 'Twenty', 'Ten', 'Five,', 'One']
+  currency = [100,50,20,10,5,1,.25,.10,.05,.01]
 
   amount_due = amount_paid - item_cost
-  holding_ground = []
+  holding_ground = f'The optimal change for an item that costs ${item_cost} with an amount paid of {amount_paid} is '
 
   if amount_due > 0:
     for i in range(len(currency)):
       times_how_many = math.floor(amount_due // currency[i])
+      #where you account for single/plural
       if times_how_many > 0:
-        print(times_how_many)
+        holding_ground += f'{times_how_many} ${currency[i]} bill '
         amount_due -= times_how_many * currency[i]
-
-print(optimal_change(10, 20))
+       
+  return holding_ground
+print(optimal_change(10, 20.75))
